59e428c72b6675ca1fedb8856428d68098056be3
Michael Papp <mikekppp@aim.com>
2010-09-09 01:30:19 -0700

    removing build directory

--- S4LuceneLib/build/S4LuceneLib.build/Release-iphoneos/S4LuceneLib.build/Script-0A3BC68A11FC2BCE001D4BCA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -e "build/Release-iphoneos/libS4LuceneLib.a" ] ; then
-	if [ -e "build/Debug-iphonesimulator/libS4LuceneLib.a" ] ; then
-		lipo -create "build/Release-iphoneos/libS4LuceneLib.a" "build/Debug-iphonesimulator/libS4LuceneLib.a" -output "dist/libS4LuceneLib.a"
-		cp Classes/Lucene/*.h dist/Headers/
-		cp Resources/* dist/Resources
-	fi
-fi
-
